a2cefa236565004c896a5ab311b0d63404a8ae72
Beau Elliott <beauelliott@Beaus-MacBook-Pro.local>
2024-02-21 11:05:25 -0800

    Start code with all packages updated and basic configs

---
+++ tailwind.config.ts
@@ -0,0 +1,33 @@
+import daisyui from "daisyui";
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#00deec",
+          secondary: "#0056d9",
+          accent: "#00e48c",
+          neutral: "#070403",
+          "base-100": "#26222c",
+          info: "#00f7ff",
+          success: "#00c392",
+          warning: "#c58000",
+          error: "#ba1c2d",
+          body: {
+            //this way to add a dark theme with different background color
+            "background-color": "#e3e6e6",
+          },
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
+export default config;
