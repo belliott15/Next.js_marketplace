0b17662a51d5020e5f680ac991f040678300e1d9
Beau Elliott <beauelliott@Beaus-MacBook-Pro.local>
2024-02-27 15:04:58 -0800

    added favicons and icons for web application

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,6 +2,7 @@ import { getCart } from "@/lib/db/cart";
 import CartEntry from "./CartEntry";
 import { setProductQuantity } from "./actions";
 import { formatPrice } from "@/lib/format";
+import CheckoutButton from "./CheckoutButton";
 
 export const metadata = {
   title: "Your Cart - Fern & Frond",
@@ -30,7 +31,7 @@ export default async function cartPage() {
         <p className="mb-3 font-bold">
           Total: {formatPrice(cart?.subtotal || 0)}
         </p>
-        <button className="btn btn-primary max-w-[100px]">Checkout</button>
+        <CheckoutButton />
       </div>
     </div>
   );
