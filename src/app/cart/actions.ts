6924fee3bfbd82244a6c0e25891b9c06c6a45bf9
Beau Elliott <beauelliott@Beaus-MacBook-Pro.local>
2024-02-27 12:59:04 -0800

    updated all database calls to update cart instead of cart items

--- src/app/cart/actions.ts
+++ src/app/cart/actions.ts
@@ -11,25 +11,42 @@ export async function setProductQuantity(productId: string, quantity: number) {
 
   if (quantity === 0) {
     if (itemInCart) {
-      await prisma.cartItem.delete({
-        where: { id: itemInCart.id },
-      });
+      await prisma.cart.update({
+        where: { id: cart.id }, 
+        data: {
+          items: {
+            delete: {
+              id: itemInCart.id
+            }
+          }
+        }
+      })
+      
     }
   } else {
     if (itemInCart) {
-      await prisma.cartItem.update({
-        where: { id: itemInCart.id },
-        data: { quantity },
-      });
-    } else {
-      await prisma.cartItem.create({
+      await prisma.cart.update({
+        where: {id: cart.id}, 
         data: {
-          //@ts-ignore
-          cartId: cart.id,
-          productId,
-          quantity,
-        },
-      });
+          items: {
+            update: {
+              where: {id: itemInCart.id}, 
+              data: {quantity}
+            }
+          }
+        }
+      })
+
+    } else {
+      await prisma.cart.update({
+        where: {id: cart.id}, 
+        data: {
+          items: {create: { 
+            productId, 
+            quantity
+          }}
+        }
+      })
     }
   }
 
--- src/components/PaginationBar.tsx
+++ src/components/PaginationBar.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { number } from "zod";
 
 interface PaginationBarProps {
   currentPage: number; 
